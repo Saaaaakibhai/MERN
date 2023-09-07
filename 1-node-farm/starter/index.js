9dfe28cc4c1c02d4b38f4b1e12fee94f2917b681
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2023-09-07 23:25:25 +0600

    mern

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,10 +1,35 @@
 const fs = require('fs');
 
-/*console.log("Hello World");*/
+//Blocking,Synchronous way//
 
+/*console.log("Hello World");*/
+/* 
 const textln = fs.readFileSync('./txt/input.txt','utf-8');
 console.log(textln);
 /*'this is what avocado: ${textln}. \n Created on ${Date.now()}';*/
-const textout = `this is really awesome: ${textln}.\nCreated on ${Date.now()}`;
+/*const textout = `this is really awesome: ${textln}.\nCreated on ${Date.now()}`;
 fs.writeFileSync('./txt/output.txt',textout);
-console.log('File written!');
+console.log('File written!'); */
+/*
+   
+    
+
+
+    */
+//Non-blocking, Asynchronous way
+
+
+fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+        console.log(data2);
+        fs.readFile(`./txt/append.txt`,'utf-8',(err,data3)=>{
+            console.log(data3);
+            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
+                console.log('your file has been written');
+                })
+           });
+        
+        });
+
+});
+console.log('will read file!!');
