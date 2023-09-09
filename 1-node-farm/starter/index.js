4337ebb1edbe4436f28dc9292626937d0b1c39ad
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2023-09-09 23:00:05 +0600

    mern

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -16,20 +16,22 @@ console.log('File written!'); */
 
 
     */
+
+//Another Video
 //Non-blocking, Asynchronous way
 
 
-fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
-    fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
-        console.log(data2);
-        fs.readFile(`./txt/append.txt`,'utf-8',(err,data3)=>{
-            console.log(data3);
-            fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
-                console.log('your file has been written');
-                })
-           });
+// fs.readFile('./txt/start.txt','utf-8',(err,data1)=>{
+//     fs.readFile(`./txt/${data1}.txt`,'utf-8',(err,data2)=>{
+//         console.log(data2);
+//         fs.readFile(`./txt/append.txt`,'utf-8',(err,data3)=>{
+//             console.log(data3);
+//             fs.writeFile('./txt/final.txt',`${data2}\n${data3}`,'utf-8',err=>{
+//                 console.log('your file has been written');
+//                 })
+//            });
         
-        });
+//         });
 
-});
-console.log('will read file!!');
+// });
+// console.log('will read file!!');
