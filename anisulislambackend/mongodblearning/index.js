4621923b8f3f6d2856581fc363148596d9c62077
S. M. Mahedi Hasan <69164727+Saaaaakibhai@users.noreply.github.com>
2024-01-11 17:16:04 +0600

    anisulislam

---
+++ anisulislambackend/mongodblearning/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const mongoose = require('mongoose');
+
+const app = express();
+
+const port = 3002;
+// async await issue 
+const connectDB = async () => {
+    try {
+        await mongoose.connect('mongodb://127.0.0.1:27017/testProductDB');
+        console.log("db is connected");
+    } catch (error) {
+        console.log("db is not connected");
+        console.log(error.message);
+        process.exit(1);
+    }   
+}
+
+// than catch issue
+// mongoose.connect('mongodb://127.0.0.1:27017/testProductDB')
+//     .then(() => console.log("db is connected"))
+//     .catch((error) => {
+//         console.log("DB is not connected");
+//         console.log(Error404);
+//         process.exit(1);
+//     });
+
+app.listen(port, async () => {
+    console.log(`server is running at http://localhost:${port}`);
+    await connectDB();
+});
+
+app.get("/", (req, res) => {
+    res.send("welcome brother");
+});
